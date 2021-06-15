dcecfa9c186172e7c6ec3c563043112242e7e730
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2021-06-15 23:00:17 +0200

    Example 1.3 started

--- ch01_introduction.R
+++ ch01_introduction.R
@@ -48,3 +48,59 @@ nh_ts - c_ts
 ## Example 1.2 - President’s Heights
 #-------------------------------------------------------------------------------
 
+
+winner <- c(185, 182, 182, 188,
+           188, 188, 185, 185,
+           177, 182, 182, 193,
+           183, 179, 179, 175)
+
+opponent <- c(175, 193, 185, 187,
+              188, 173, 180, 177,
+              183, 185, 180, 180,
+              182, 178, 178, 173)
+
+length(winner)
+
+# correction of Bill Clinton's height
+winner[4:5] <- 189
+
+# election years
+year <- seq(from=2008, to=1948, by=-4)
+
+# mean height
+mean(winner)
+mean(opponent)
+
+# calculate height difference
+difference <- winner - opponent
+
+# create a data frame
+data.frame(year, winner, opponent, difference)
+
+# create a logical variable
+taller.won <- winner > opponent
+
+# how often did the taller win?
+table(taller.won)
+
+# percentage
+table(taller.won) / length(taller.won) * 100
+
+# barplot of height differences
+barplot(rev(difference),
+        xlab = "Election years 1948 to 2008",
+        ylab = "Height difference in cm")
+
+
+
+#-------------------------------------------------------------------------------
+## Example 1.3 - Fatal Horsekicks at Prussian Cavalry
+#-------------------------------------------------------------------------------
+
+
+k <- c(0, 1, 2, 3, 4)
+x <- c(109, 65, 22, 3, 1)
+
+barplot(x, names.arg=k,
+        xlab = "Number of fatalities per year and corps",
+        ylab = "Number of corps years")
